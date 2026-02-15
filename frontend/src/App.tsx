df1e29deb617522eb0f4b7fd6141a8ef0dd43f20
Laszlo Lazuer <visualjazz@gmail.com>
2026-02-15 10:30:30 -0700

    chore: add backend rest endpoint and call from frontend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+interface Greeting {
+  id: number;
+  content: string;
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  const [data, setData] = useState<Greeting | null>(null);
+
+  useEffect(()=> {
+  const load = async () => {
+    const res = await fetch('http://localhost:8080/greeting')
+    const data = await res.json()
+    console.log(data)
+    setData(data);
+  }
+  load()
+
+  }, [])
 
   return (
     <>
@@ -17,13 +33,17 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div>
+        <p>ID: {data?.id}</p>
+        <p>Content: {data?.content}</p>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        {data && <p>
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </p>}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
